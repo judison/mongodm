652ce4c4e384b3b4fe2ef00e6a32cc6a90b08c5b
Judison Oliveira Gil Filho <judison@gmail.com>
2013-04-15 16:58:50 +0000

    Added Update Object

---
+++ mongodm/src/org/judison/mongodm/Update.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright (c) 2013, Judison Oliveira Gil Filho <judison@gmail.com>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice,
+ *    this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. The name of the author may not be used to endorse or promote products
+ *    derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+package org.judison.mongodm;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DBObject;
+
+public class Update {
+
+	private BasicDBObject update = new BasicDBObject();
+
+	public Update() {}
+
+	public Update put(String name, Object value) {
+		update.put(name, value);
+		return this;
+	}
+	
+	private void putIn(String operator, String field, Object value) {
+		DBObject sub = (DBObject)update.get(operator);
+		if (sub == null) {
+			sub = new BasicDBObject();
+			update.put(operator, sub);
+		}
+		sub.put(field, value);
+	}
+	
+	private Object getIn(String operator, String field) {
+		DBObject sub = (DBObject)update.get(operator);
+		if (sub == null)
+			return null;
+		return sub.get(field);
+	}
+	
+	public Update set(String field, Object value) {
+		putIn("$set", field, value);
+		return this;
+	}
+	
+	/**
+	 * The $setOnInsert operator assigns values to fields during an upsert only when using the upsert option to the update() operation performs an insert.
+	 */
+	public Update setOnInsert(String field, Object value) {
+		putIn("$setOnInsert", field, value);
+		return this;
+	}
+
+	public Update unset(String... fields) {
+		for (String field: fields)
+			putIn("$unset", field, 1);
+		return this;
+	}
+	
+	public Update push(String array, Object... values) {
+		for (Object value: values)
+			_each("$push", array, value);
+		return this;
+	}
+	
+	public Update pull(String array, Object... values) {
+		for (Object value: values)
+			_each("$pull", array, value);
+		return this;
+	}
+	
+	public Update addToSet(String array, Object... values) {
+		for (Object value: values)
+			_each("$addToSet", array, value);
+		return this;
+	}
+	
+	private void _each(String operator, String array, Object value) {
+		Object obj = getIn(operator, array);
+		if (obj == null)
+			putIn(operator, array, value);
+		else if (obj instanceof DBObject && ((DBObject)obj).containsField("$each")) {
+			DBObject eh = (DBObject)obj;
+			BasicDBList each = (BasicDBList)eh.get("$each");
+			each.add(value);
+		} else {
+			BasicDBObject eh = new BasicDBObject(); 
+			BasicDBList each = new BasicDBList();
+			each.add(obj);
+			each.add(value);
+			eh.put("$each", each);
+			putIn(operator, array, eh);
+		}
+	}
+	
+	public Update pop(String field) {
+		putIn("$pop", field, 1);
+		return this;
+	}
+	
+	public Update popFirst(String field) {
+		putIn("$pop", field, -1);
+		return this;
+	}
+	
+	public Update rename(String oldName, String newName) {
+		putIn("$rename", oldName, newName);
+		return this;
+	}
+
+	public DBObject toDBObject() {
+		return update;
+	}
+
+	@Override
+	public String toString() {
+		return update.toString();
+	}
+
+}
